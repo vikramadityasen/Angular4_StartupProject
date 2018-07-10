1b711303a3b0cf3d8c5fe276a80beb36551e7da3
unknown <sen.vikram5@gmail.com>
2018-07-10 21:02:54 +0530

    implemented months in drop down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular 4 Project';
+  // declared array of months.
+  months = ["January", "Feburary", "March", "April", "May", 
+  "June", "July", "August", "September",
+  "October", "November", "December"];
 }
